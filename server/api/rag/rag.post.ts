dbbb8239d3125232616089c5727e0da60d51dca5
mikechao <mike.chao.one@gmail.com>
2025-03-17 12:55:43 -0700

    Converting retrieve to tool

--- server/api/rag/rag.post.ts
+++ server/api/rag/rag.post.ts
@@ -1,11 +1,13 @@
 import type { Document } from '@langchain/core/documents'
 import { PGVectorStore } from '@langchain/community/vectorstores/pgvector'
+import { AIMessage, HumanMessage, SystemMessage, ToolMessage } from '@langchain/core/messages'
 import { PromptTemplate } from '@langchain/core/prompts'
-import { Annotation, StateGraph } from '@langchain/langgraph'
+import { tool } from '@langchain/core/tools'
+import { Annotation, MessagesAnnotation, StateGraph } from '@langchain/langgraph'
+import { ToolNode, toolsCondition } from '@langchain/langgraph/prebuilt'
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import consola from 'consola'
 import pg from 'pg'
-
 import { z } from 'zod'
 
 export default defineLazyEventHandler(async () => {
@@ -43,44 +45,91 @@ export default defineLazyEventHandler(async () => {
     apiKey: openaiAPIKey,
   })
 
-  const promptTemplate = PromptTemplate.fromTemplate(`You are an assistant for question-answering tasks. 
-  Use the following pieces of retrieved context to answer the question. 
-  If you don't know the answer, just say that you don't know. 
-  Use three sentences maximum and keep the answer concise.
-  Question: {question} 
-  Context: {context} `)
-
-  // Define state for application
-  const InputStateAnnotation = Annotation.Root({
-    question: Annotation<string>,
+  const retrieveToolSchema = z.object({
+    query: z.string(),
   })
 
-  const StateAnnotation = Annotation.Root({
-    question: Annotation<string>,
-    context: Annotation<Document[]>,
-    answer: Annotation<string>,
-  })
+  const retrieveTool = tool(
+    async ({ query }) => {
+      const retrievedDocs = await vectorStore.similaritySearch(query, 2)
+      const serialized = retrievedDocs
+        .map(
+          doc => `Source: ${doc.metadata.source}\nContent: ${doc.pageContent}`,
+        )
+        .join('\n')
+      return [serialized, retrievedDocs]
+    },
+    {
+      name: 'retrieve',
+      description: 'Retrieve information related to a query.',
+      schema: retrieveToolSchema,
+      responseFormat: 'content_and_artifact',
+    },
+  )
 
-  // Define application steps
-  const retrieve = async (state: typeof InputStateAnnotation.State) => {
-    const retrievedDocs = await vectorStore.similaritySearch(state.question)
-    return { context: retrievedDocs }
+  async function queryOrRespond(state: typeof MessagesAnnotation.State) {
+    const llmWithTools = llm.bindTools([retrieveTool])
+    const response = await llmWithTools.invoke(state.messages)
+    // MessagesState appends messages to state instead of overwriting
+    return { messages: [response] }
   }
 
-  const generate = async (state: typeof StateAnnotation.State) => {
-    const docsContent = state.context.map(doc => doc.pageContent).join('\n')
-    const messages = await promptTemplate.invoke({ question: state.question, context: docsContent })
-    const response = await llm.invoke(messages)
-    return { answer: response.content }
+  const tools = new ToolNode([retrieveTool])
+
+  async function generate(state: typeof MessagesAnnotation.State) {
+    // Get generated ToolMessages
+    const recentToolMessages = []
+    for (let i = state.messages.length - 1; i >= 0; i--) {
+      const message = state.messages[i]
+      if (message instanceof ToolMessage) {
+        recentToolMessages.push(message)
+      }
+      else {
+        break
+      }
+    }
+    const toolMessages = recentToolMessages.reverse()
+
+    // Format into prompt
+    const docsContent = toolMessages.map(doc => doc.content).join('\n')
+    const systemMessageContent
+      = 'You are an assistant for question-answering tasks. '
+        + 'Use the following pieces of retrieved context to answer '
+        + 'the question. If you don\'t know the answer, say that you '
+        + 'don\'t know. Use three sentences maximum and keep the '
+        + 'answer concise.'
+        + '\n\n'
+        + `${docsContent}`
+
+    const conversationMessages = state.messages.filter(
+      message =>
+        message instanceof HumanMessage
+        || message instanceof SystemMessage
+        || (message instanceof AIMessage && message.tool_calls?.length === 0),
+    )
+    const prompt = [
+      new SystemMessage(systemMessageContent),
+      ...conversationMessages,
+    ]
+
+    // Run
+    const response = await llm.invoke(prompt)
+    return { messages: [response] }
   }
 
-  const graph = new StateGraph(StateAnnotation)
-    .addNode('retrieve', retrieve, { input: InputStateAnnotation })
+  const graphBuilder = new StateGraph(MessagesAnnotation)
+    .addNode('queryOrRespond', queryOrRespond)
+    .addNode('tools', tools)
     .addNode('generate', generate)
-    .addEdge('__start__', 'retrieve')
-    .addEdge('retrieve', 'generate')
+    .addEdge('__start__', 'queryOrRespond')
+    .addConditionalEdges('queryOrRespond', toolsCondition, {
+      __end__: '__end__',
+      tools: 'tools',
+    })
+    .addEdge('tools', 'generate')
     .addEdge('generate', '__end__')
-    .compile()
+
+  const graph = graphBuilder.compile()
 
   return defineEventHandler(async (event) => {
     const body = await readBody(event)
