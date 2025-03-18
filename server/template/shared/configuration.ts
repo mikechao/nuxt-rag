ce5ae587abc10759d71080480ef2c49323a78d7c
mikechao <mike.chao.one@gmail.com>
2025-03-18 15:22:10 -0700

    Added rag research agent template

---
+++ server/template/shared/configuration.ts
@@ -0,0 +1,53 @@
+/**
+ * Define the configurable parameters for the agent.
+ */
+
+import type { RunnableConfig } from '@langchain/core/runnables'
+import { Annotation } from '@langchain/langgraph'
+
+/**
+ * typeof ConfigurationAnnotation.State class for indexing and retrieval operations.
+ *
+ * This annotation defines the parameters needed for configuring the indexing and
+ * retrieval processes, including user identification, embedding model selection,
+ * retriever provider choice, and search parameters.
+ */
+export const BaseConfigurationAnnotation = Annotation.Root({
+  /**
+   * Name of the embedding model to use. Must be a valid embedding model name.
+   */
+  embeddingModel: Annotation<string>,
+
+  /**
+   * The vector store provider to use for retrieval.
+   * Options are 'elastic', 'elastic-local', 'pinecone', or 'mongodb'.
+   */
+  retrieverProvider: Annotation<
+    'elastic' | 'elastic-local' | 'pinecone' | 'mongodb' | 'pgvector'
+  >,
+
+  /**
+   * Additional keyword arguments to pass to the search function of the retriever.
+   */
+  searchKwargs: Annotation<Record<string, any>>,
+})
+
+/**
+ * Create an typeof BaseConfigurationAnnotation.State instance from a RunnableConfig object.
+ *
+ * @param config - The configuration object to use.
+ * @returns An instance of typeof BaseConfigurationAnnotation.State with the specified configuration.
+ */
+export function ensureBaseConfiguration(
+  config: RunnableConfig,
+): typeof BaseConfigurationAnnotation.State {
+  const configurable = (config?.configurable || {}) as Partial<
+    typeof BaseConfigurationAnnotation.State
+  >
+  return {
+    embeddingModel:
+      configurable.embeddingModel || 'openai/text-embedding-3-small',
+    retrieverProvider: configurable.retrieverProvider || 'pgvector',
+    searchKwargs: configurable.searchKwargs || {},
+  }
+}
---
+++ server/template/shared/retrieval.ts
@@ -0,0 +1,67 @@
+import type { Embeddings } from '@langchain/core/embeddings'
+import type { RunnableConfig } from '@langchain/core/runnables'
+import type { VectorStoreRetriever } from '@langchain/core/vectorstores'
+import { PGVectorStore } from '@langchain/community/vectorstores/pgvector'
+import { OpenAIEmbeddings } from '@langchain/openai'
+import pg from 'pg'
+import { ensureBaseConfiguration } from './configuration.js'
+// https://github.com/langchain-ai/rag-research-agent-template-js/blob/main/src/shared/retrieval.ts
+
+async function makePgVectorRetriever(configuration: ReturnType<typeof ensureBaseConfiguration>, embeddingModel: Embeddings): Promise<VectorStoreRetriever> {
+  const { Pool } = pg
+  const pool = new Pool({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'dbpassword',
+    database: 'nuxtragdb',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
+
+  const vectorStore = await PGVectorStore.initialize(embeddingModel, {
+    pool,
+    tableName: 'rag_vectors',
+  })
+  return vectorStore.asRetriever({ filter: configuration.searchKwargs || {} })
+}
+
+function makeTextEmbeddings(modelName: string): Embeddings {
+  /**
+   * Connect to the configured text encoder.
+   */
+  const index = modelName.indexOf('/')
+  let provider, model
+  if (index === -1) {
+    model = modelName
+    provider = 'openai' // Assume openai if no provider included
+  }
+  else {
+    provider = modelName.slice(0, index)
+    model = modelName.slice(index + 1)
+  }
+  const runtimeConfig = useRuntimeConfig()
+  const openaiAPIKey = runtimeConfig.openaiAPIKey
+  switch (provider) {
+    case 'openai':
+      return new OpenAIEmbeddings({ model, apiKey: openaiAPIKey })
+    default:
+      throw new Error(`Unsupported embedding provider: ${provider}`)
+  }
+}
+
+export async function makeRetriever(
+  config: RunnableConfig,
+): Promise<VectorStoreRetriever> {
+  const configuration = ensureBaseConfiguration(config)
+  const embeddingModel = makeTextEmbeddings(configuration.embeddingModel)
+  switch (configuration.retrieverProvider) {
+    case 'pgvector':
+      return await makePgVectorRetriever(configuration, embeddingModel)
+    default:
+      throw new Error(
+        `Unrecognized retrieverProvider in configuration: ${configuration.retrieverProvider}`,
+      )
+  }
+}
