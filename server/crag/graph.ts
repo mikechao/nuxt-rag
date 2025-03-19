6681de2c607403540b0392d9959481b952cfa811
mikechao <mike.chao.one@gmail.com>
2025-03-19 14:09:28 -0700

    Fix issues with creating embeddings and vectorstore for crag

--- server/crag/graph.ts
+++ server/crag/graph.ts
@@ -2,23 +2,47 @@ import type { DocumentInterface } from '@langchain/core/documents'
 import type { BaseChatModel } from '@langchain/core/language_models/chat_models'
 import type { RunnableConfig } from '@langchain/core/runnables'
 import { TavilySearchResults } from '@langchain/community/tools/tavily_search'
+import { PGVectorStore } from '@langchain/community/vectorstores/pgvector'
 import { Document } from '@langchain/core/documents'
 import { StringOutputParser } from '@langchain/core/output_parsers'
 import { ChatPromptTemplate } from '@langchain/core/prompts'
 import { END, START, StateGraph } from '@langchain/langgraph'
-import { ChatOpenAI } from '@langchain/openai'
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import consola from 'consola'
 import { formatDocumentsAsString } from 'langchain/util/document'
+import pg from 'pg'
 import { z } from 'zod'
-import { createEmbeddingsAndVectorStore } from '../util/embeddingAndVectorStore'
 import { GraphState } from './state'
 
 // https://langchain-ai.github.io/langgraphjs/tutorials/rag/langgraph_crag
 
+async function makeEmbeddings() {
+  const embeddings = new OpenAIEmbeddings({
+    model: 'text-embedding-3-large',
+    dimensions: 3072,
+  })
+  return embeddings
+}
+
 async function makeRetriever() {
-  const { vectorStore } = await createEmbeddingsAndVectorStore()
-  const retriever = vectorStore.asRetriever()
-  return retriever
+  const embeddings = await makeEmbeddings()
+  const { Pool } = pg
+  const pool = new Pool({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'dbpassword',
+    database: 'nuxtragdb',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
+  const vectorStore = await PGVectorStore.initialize(embeddings, {
+    pool,
+    tableName: 'rag_vectors',
+    dimensions: 3072,
+  })
+  return vectorStore.asRetriever()
 }
 
 async function makeModel(): Promise<BaseChatModel> {
